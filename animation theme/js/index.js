3f8120d46f2af2f07fe0efac8f34da91670f67e0
Akshay1995 <akshay1995.1@gmail.com>
2017-08-23 00:14:23 +0530

    animation theme

---
+++ animation theme/js/index.js
@@ -0,0 +1,4 @@
+//  another one stunning illustration by my lovely Lina, another one milestone for our team's dribbble account, so I decided to give it  a shot and animate this one in a classic css/svg  way.
+// everything here is prepared in illustrator and exported to svg 
+// fire sequence is hand-crafted by Lina also,  animated simply by using svg sprite and <foreignobject>
+// no real known issues except my one and only, loving Firefox. I really love this browser, but it is a real pain to deal with svg animations and filters in it.  Firefox deals strangely with filters and foreignobject here, I know.
